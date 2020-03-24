6c6731849f048cda11a8bead62e2891c4a0f9052
limeunhee <53496938+limeunhee@users.noreply.github.com>
2020-03-24 15:12:12 -0700

    Create 1295.py

---
+++ 1295.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+    
+        count=0
+        for i in range(len(nums)):
+            if len(str(nums[i]))%2==0:
+                count= count+1
+       
+       return count
