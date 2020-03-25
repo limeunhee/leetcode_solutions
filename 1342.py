b7326b5e9115fbe5fedfbdb90b259d04aab5865a
limeunhee <53496938+limeunhee@users.noreply.github.com>
2020-03-24 19:25:53 -0700

    Create 1342.py

---
+++ 1342.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+              
+        a=num
+        count = 0
+        
+        while a != 0:
+            if (a%2)==0:
+                a = a/2
+                count = count +1
+            elif (a%2)==1:
+                a= a-1
+                count = count +1
+        
+        return count
