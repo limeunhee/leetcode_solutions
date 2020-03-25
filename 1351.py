80924a0d4ddfcf8db4dc26648a52830fc0ebf099
limeunhee <53496938+limeunhee@users.noreply.github.com>
2020-03-24 18:20:04 -0700

    Update 1351.py

--- 1351.py
+++ 1351.py
@@ -1,8 +1,4 @@
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        
-        count=0
-        for i in grid:
-            count += (len(list(filter(lambda x: (x < 0), i))) )
-        
-        return count
+    def countNegatives(self, grid: List[List[int]]) -> int:        
+        new_list = [i for x in grid for i in x]
+        return sum([num<0 for num in new_list])
